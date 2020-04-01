86d0a8145f747faaf832eae5c104fccbcffa8c67
gargisharma1998 <60867073+gargisharma1998@users.noreply.github.com>
2020-04-01 19:59:21 +0530

    Update color.groovy

--- vars/color.groovy
+++ vars/color.groovy
@@ -8,18 +8,6 @@ def loadColors(){
    ON_YELLOW='\033[43m'
    NC='033[0m'
 }
-def jobs = hudson.model.Hudson.instance.items
-jobs.each { job ->
-    def has_color_wrapper = false
-    job.buildWrappersList.each { wrapper ->
-      if (wrapper instanceof hudson.plugins.ansicolor.AnsiColorBuildWrapper) {
-        has_color_wrapper = true
-        println("$job.name is already configured.")
-      }
-    }
-    if (!has_color_wrapper) {
-        println("Adding AnsiColor to $job.name")
-        job.buildWrappersList.add(new hudson.plugins.ansicolor.AnsiColorBuildWrapper("xterm"));
-        job.save()
-    }
+options{
+   ansicolor('xterm')
 }
